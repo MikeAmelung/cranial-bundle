f36478b92d981dad858c9736466b8bd2c902cbdd
Mike Amelung <mike@mikeamelung.com>
2021-02-01 10:40:26 -0600

    got it this time

--- Command/DupeContentCommand.php
+++ Command/DupeContentCommand.php
@@ -8,7 +8,7 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-use MikeAmelung\CranialBundle\Service\ContentManager;
+use MikeAmelung\CranialBundle\ContentManager\ContentManager;
 
 class DupeContentCommand extends Command
 {
