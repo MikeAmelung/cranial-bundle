aea9f20eb52d4f8c34371a9c08b4e8a154587afe
Mike Amelung <mike@mikeamelung.com>
2025-08-14 10:30:08 -0500

    add video-container class to video embeds

--- src/Utils/Markdown.php
+++ src/Utils/Markdown.php
@@ -2,14 +2,18 @@
 
 namespace MikeAmelung\CranialBundle\Utils;
 
+use Embed\Embed;
 use League\CommonMark\Environment\Environment;
 use League\CommonMark\Extension\CommonMark\CommonMarkCoreExtension;
 use League\CommonMark\Extension\Embed\Bridge\OscaroteroEmbedAdapter;
+use League\CommonMark\Extension\Embed\Embed as EmbedNode;
 use League\CommonMark\Extension\Embed\EmbedExtension;
+use League\CommonMark\Extension\Embed\EmbedRenderer;
 use League\CommonMark\Extension\GithubFlavoredMarkdownExtension;
 use League\CommonMark\Extension\InlinesOnly\InlinesOnlyExtension;
 use League\CommonMark\Extension\Attributes\AttributesExtension;
 use League\CommonMark\MarkdownConverter;
+use League\CommonMark\Renderer\HtmlDecorator;
 
 class Markdown
 {
@@ -18,10 +22,18 @@ class Markdown
 
     public function __construct()
     {
+        $embedLibrary = new Embed();
+        $embedLibrary->setSettings([
+            'oembed:query_parameters' => [
+                'maxwidth' => 1280,
+                'maxheight' => 720,
+            ],
+        ]);
+
         $config = [
             'allow_unsafe_links' => false,
             'embed' => [
-                'adapter' => new OscaroteroEmbedAdapter(),
+                'adapter' => new OscaroteroEmbedAdapter($embedLibrary),
                 'allowed_domains' => ['youtube.com', 'vimeo.com'],
                 'fallback' => 'link',
             ],
@@ -33,6 +45,8 @@ class Markdown
         $environment->addExtension(new GithubFlavoredMarkdownExtension());
         $environment->addExtension(new AttributesExtension());
         $environment->addExtension(new EmbedExtension());
+
+        $environment->addRenderer(EmbedNode::class, new HtmlDecorator(new EmbedRenderer(), 'div', ['class' => 'video-container']));
 
         $this->converter = new MarkdownConverter($environment);
 
